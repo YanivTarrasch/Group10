caced97caf25cd958c4b67627ca1b2ba66f71879
chenaz1 <57365377+chenaz1@users.noreply.github.com>
2020-01-14 20:51:48 +0200

    getConnect

--- DB.py
+++ DB.py
@@ -1,3 +1,8 @@
 #Import the TinyDB module and submodules
 from tinydb import TinyDB, Query
  
+
+#Declare our database variable and the file to store our data in
+def getConnection(str):
+    db = TinyDB(str+'.json')
+    return db
