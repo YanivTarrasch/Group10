b89059a1e688ab7a1399737cf8df48c9f9a21752
yaniv098 <58630354+yaniv098@users.noreply.github.com>
2020-01-14 19:50:08 +0200

    parent view function done

--- FULL_GUI.py
+++ FULL_GUI.py
@@ -11,3 +11,12 @@ def parentView(parent):
     window=Tk()
    
     id=parent.id
+    b1 = Button(window, text="Make Question", width=12,bg="gray", command=lambda:Add_Question())
+    #b1.place(relx=0, rely=0,x=-50,y=15)
+    b1.grid(row=1,column=0)
+    b2 = Button(window, text="LogOut", width=12,bg="blue", command=lambda:(window.destroy()))
+    #b2.place(relx=1,rely=0,x=-50,y=15)
+    b2.grid(row=1,column=1 )
+    p=tb.search((query.User=='Player')&(query.ParentID==parent.id))
+    showStats(window,p)
+    window.mainloop()
